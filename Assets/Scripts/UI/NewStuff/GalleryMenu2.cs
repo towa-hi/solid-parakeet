8be58fe7eda9ed7ea7a3647b0d913db2b087eefe
towa-hi <yamyamasunday@gmail.com>
2025-10-01 22:10:02 +0900

    gallery work

--- Assets/Scripts/UI/NewStuff/GalleryMenu2.cs
+++ Assets/Scripts/UI/NewStuff/GalleryMenu2.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class GalleryMenu2 : MenuBase
 {
     public Button backButton;
 
+    public Button teamButton;
+    public Button prevRankButton;
+    public Button nextRankButton;
+
+    public Card currentCard;
+
+    public List<Card> cards;
+
+    public Transform cardRoot;
+    
+    public Team currentTeam;
+    public Rank currentRank;
+
+    
     private void Start()
     {
         backButton.onClick.AddListener(HandleBack);
